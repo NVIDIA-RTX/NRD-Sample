22587ba6887e2be42999b89eff841859a7c3c326
dzhdan <dzhdan@nvidia.com>
2025-05-15 18:37:50 +0800

    Updated scripts

--- 4-Clean.bat
+++ 4-Clean.bat
@@ -6,6 +6,8 @@ if exist "_Bin" rd /q /s "_Bin"
 if exist "_Build" rd /q /s "_Build"
 if exist "_Data" rd /q /s "_Data"
 if exist "_Shaders" rd /q /s "_Shaders"
+if exist "_NRD_SDK" rd /q /s "_NRD_SDK"
+if exist "_NRI_SDK" rd /q /s "_NRI_SDK"
 
 cd "External/NRIFramework"
 call "4-Clean.bat"
--- 4-Clean.sh
+++ 4-Clean.sh
@@ -6,6 +6,8 @@ rm -rf "_Bin"
 rm -rf "_Build"
 rm -rf "_Data"
 rm -rf "_Shaders"
+rm -rf "_NRD_SDK"
+rm -rf "_NRI_SDK"
 
 cd "External/NRIFramework"
 source "4-Clean.sh"
