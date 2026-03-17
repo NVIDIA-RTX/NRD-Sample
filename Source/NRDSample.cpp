a21d3d16919f565efda1e8dc5c25b0fd8b107368
dzhdan <dzhdan@nvidia.com>
2026-03-17 13:43:54 +0800

    DLSS: switched to preset M due to specular reprojection issues with the default preset

--- Source/NRDSample.cpp
+++ Source/NRDSample.cpp
@@ -33,7 +33,7 @@ constexpr bool CAMERA_RELATIVE = true;
 constexpr bool ALLOW_BLAS_MERGING = true;
 constexpr bool ALLOW_HDR = NRIF_PLATFORM == NRIF_WINDOWS; // use "WIN + ALT + B" to switch HDR mode
 constexpr bool USE_LOW_PRECISION_FP_FORMATS = true;       // saves a bit of memory and performance
-constexpr uint8_t DLSS_PRESET = 0;
+constexpr uint8_t DLSS_PRESET = 13; // preset M(13) (expensive, correct specular tracking, default preset is broken), the alternative is F(6) (CNN, correct specular tracking)
 constexpr nri::UpscalerType upscalerType = nri::UpscalerType::DLSR;
 constexpr int32_t MAX_HISTORY_FRAME_NUM = (int32_t)std::min(60u, std::min(nrd::REBLUR_MAX_HISTORY_FRAME_NUM, nrd::RELAX_MAX_HISTORY_FRAME_NUM));
 constexpr uint32_t TEXTURES_PER_MATERIAL = 4;
